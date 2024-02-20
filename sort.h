838cdbc6d65f8545c5305d493ad94bce6caf8945
easydigs <easydigitalkoncept@gmail.com>
2024-02-20 23:42:10 +0100

    Task 5 Cocktail shaker sort

--- sort.h
+++ sort.h
@@ -32,5 +32,6 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif /*SORT_H*/
