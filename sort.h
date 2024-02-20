f1ab51e26b8def713ca78f3805b9cdd05a49e43d
easydigs <easydigitalkoncept@gmail.com>
2024-02-20 23:30:10 +0100

    Task 3 Quick sort

--- sort.h
+++ sort.h
@@ -30,6 +30,7 @@ void insertion_sort_list(listint_t **list);
 void swap_ints(int *a, int *b);
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
 #endif /*SORT_H*/
